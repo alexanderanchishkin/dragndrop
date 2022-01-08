1b5da9f4cac2bed06c890866b87dfe038dda6385
ap.anchishkin <ap.anchishkin@tensor.ru>
2022-01-08 21:43:54 +0300

    Init drag and drop feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const onDragStart = (event, id) => {
+    console.log('onDragStart:', id);
+  };
+
+  const onDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const onDrop = (event) => {
+    console.log('dropped');
+  };
+
+  const colors = ['green', 'blue'];
+  const blocks = colors.map((color) => (
+    <div
+      key={color}
+      className='block'
+      draggable
+      onDragStart={(event) => onDragStart(event, color)}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+      style={{backgroundColor: color, width: '20em'}}
+    >
+      {color}
+    </div>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onDragOver={onDragOver}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {blocks}
         <a
           className="App-link"
           href="https://reactjs.org"
